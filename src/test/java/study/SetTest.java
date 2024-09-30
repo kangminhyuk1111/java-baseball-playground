7f6106e93a1f25bc48c1a3874a4767142c21f55a
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-09-30 16:52:56 +0900

    Test: Set Collection에 대한 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        final int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,1,2,3})
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_isContains_true_or_false(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
+}
