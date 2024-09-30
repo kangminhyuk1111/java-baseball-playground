3757768e97c85ae815f979f0c010f03c07b5afe7
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-09-30 16:30:32 +0900

    Test: String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,38 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        final String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
+    }
+
+    @Test
+    void split2() {
+        final String[] actual = "1".split(",");
+        assertThat(actual).contains("1");
+    }
+
+    @Test
+    void substring() {
+        final String before = "(1,2)";
+        final String actual = before.substring(1,before.length()-1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        final String string = "abc";
+        assertThrows(IndexOutOfBoundsException.class, () -> string.charAt(4));
     }
 }
