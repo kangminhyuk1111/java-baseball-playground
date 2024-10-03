e4cb00897e25dadbda4836f66ad5d4ca9c650968
kangminhyuk1111 <rkdglqkr12@naver.com>
2024-10-03 20:34:13 +0900

    feat: validation 검증 - 중복값, 숫자 3자리

--- src/main/java/baseball/ValidationUtils.java
+++ src/main/java/baseball/ValidationUtils.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ValidationUtils {
 
     public static final int MIN_VALUE = 0;
@@ -7,5 +10,24 @@ public class ValidationUtils {
 
     public static boolean validNo(int no) {
         return no > MIN_VALUE && no < MAX_VALUE;
+    }
+
+    public static boolean validNoDuplication(int balls) {
+
+        String numbers = String.valueOf(balls);
+        Set<Character> set = new HashSet<>();
+        char[] numbersCharArray = numbers.toCharArray();
+
+        for (char num : numbersCharArray) {
+            if (!set.add(num)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean validLengthThree(int balls) {
+        return String.valueOf(balls).length() == 3;
     }
 }
--- src/test/java/baseball/ValidationUtilTest.java
+++ src/test/java/baseball/ValidationUtilTest.java
@@ -5,11 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ValidationUtilTest {
+
     @Test
     void 숫자_1_9_검증() {
         assertThat(ValidationUtils.validNo(9)).isTrue();
         assertThat(ValidationUtils.validNo(1)).isTrue();
         assertThat(ValidationUtils.validNo(0)).isFalse();
         assertThat(ValidationUtils.validNo(10)).isFalse();
+    }
+
+    @Test
+    void 중복값_존재하는지_검증() {
+        assertThat(ValidationUtils.validNoDuplication(123)).isTrue();
+        assertThat(ValidationUtils.validNoDuplication(234)).isTrue();
+        assertThat(ValidationUtils.validNoDuplication(233)).isFalse();
+        assertThat(ValidationUtils.validNoDuplication(777)).isFalse();
+    }
+
+    @Test
+    void 숫자가_3자리인지() {
+        assertThat(ValidationUtils.validLengthThree(123)).isTrue();
+        assertThat(ValidationUtils.validLengthThree(456)).isTrue();
+        assertThat(ValidationUtils.validLengthThree(444)).isTrue();
+        assertThat(ValidationUtils.validLengthThree(1234)).isFalse();
     }
 }
